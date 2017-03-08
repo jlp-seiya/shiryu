f7b6279186ce3f4d3b0107649f0718e15f5cb161
John Liang <jliang@learnvest.com>
2017-03-08 18:23:17 -0500

    some WIP to refactor mydiff

--- json_diff/mydiff.py
+++ json_diff/mydiff.py
@@ -3,136 +3,175 @@ import itertools
 def diff(a, b):
     aout = []
     bout = []
-    diff_recursive(a, b, aout, bout)
-    for ao, bo in itertools.izip_longest(aout, bout):
-        print("%-20s  |  %-20s" % (ao, bo))
+    diff_recursive(a, b, aout, bout, 0)
 
-def diff_recursive(a, b, aout, bout):
+    output = []
+    for ao, bo in itertools.izip_longest(aout, bout):
+        output.append("%-20s  |  %-20s" % (ao, bo))
+
+    for o in output:
+        print(o)
+
+def diff_recursive(a, b, aout, bout, indent):
     a_type = type(a)
     b_type = type(b)
 
+    space = ' ' * indent
     if a_type != b_type:
-        aout.append("===DIFFTYPE===")
-        bout.append("===DIFFTYPE===")
-        aline = [0]
-        bline = [0]
-        to_string(a, aout, aline)
-        to_string(b, bout, bline)
-        if aline[0] > bline[0]:
-            for i in range(0, aline[0] - bline[0]):
-                bout.append('')
-        else:
-            for i in range(0, bline[0] - aline[0]):
-                aout.append('')
-        aout.append("===DIFFTYPE===")
-        bout.append("===DIFFTYPE===")
+        add_block(a, aout, indent + 1)
+        add_block(b, bout, indent + 1)
  
     elif isinstance(a, dict):
         a_keys = a.keys()
         b_keys = b.keys()
         all_keys = set().union(a_keys, b_keys)
+        first = True
+        add_begin_block("{", aout, indent)
+        add_begin_block("{", bout, indent)
         for key in all_keys:
             if key in a:
                 if key in b:
-                    aout.append('{"%s":' % key)
-                    bout.append('{"%s":' % key)
-                    diff_recursive(a[key], b[key], aout, bout)
-                    aout.append('}')
-                    bout.append('}')
+                    diff_recursive(a[key], b[key], aout, bout, indent + 1)
                 else:
-                    aout.append("===ORIGINAL===")
-                    bout.append("===BLANK===")
-                    aout.append('{"%s":' % key)
-                    bout.append('')
+                    add_block(a[key], aout, indent + 1)
+                    add_empty_block(bout)
 
-                    to_string2(a[key], aout, bout)               
-
-                    aout.append('}')
-                    bout.append('')
-                    aout.append("===ORIGINAL===")
-                    bout.append("===BLANK===")
             else:
                 if key in b:
-                    bout.append("===NEW===")
-                    aout.append("===BLANK===")
-                    bout.append('{"%s":' % key)
-                    aout.append('')
-
-                    to_string2(b[key], bout, aout)               
-
-                    bout.append('}')
-                    aout.append('')
-                    bout.append("===NEW===")
-                    aout.append("===BLANK===")
+                    add_block(b[key], bout, indent + 1)
+                    add_empty_block(aout)
                 else:
                     raise Exception("Unknow key:%s" % key)
+            first = False
+        add_end_block("}", aout, indent)
+        add_end_block("}", bout, indent)
     elif isinstance(a, list):
         a_size = len(a)
         b_size = len(b)
         max_size = max(a_size, b_size)
         min_size = min(a_size, b_size)
         for i in range(0, min_size):
-            diff_recursive(a[i], b[i], aout, bout)
+            diff_recursive(a[i], b[i], aout, bout, indent + 1)
+
+        first = (min_size == 0)
         if a_size > b_size:
             aout.append("===ORIGINAL===")
             bout.append("===BLANK===")
+
             for i in range(b_size, a_size):
-                to_string2(a[i], aout, bout)
+                to_string2(a[i], aout, bout, first)
+                first = False
             aout.append("===ORIGINAL===")
             bout.append("===BLANK===")
         else:
             bout.append("===NEW===")
             aout.append("===BLANK===")
             for i in range(a_size, b_size):
-                to_string2(b[i], bout, aout)
+                to_string2(b[i], bout, aout, first)
+                first = False
             bout.append("===NEW===")
             aout.append("===BLANK===")
     else:
-        if a == b:
-            aout.append(a)
-            bout.append(b)
+        if a != b:
+            aout.append("===DIFFVAR===")
+            bout.append("===DIFFVAR===")
+        if isinstance(a, str):
+            aout.append('%s "%s"' % (space, a))
+            bout.append('%s "%s"' % (space, b))
         else:
-            aout.append("===DIFFVAR===")
-            aout.append(a)
+            aout.append('%s %s' % (space, a))
+            bout.append('%s %s' % (space, b))
+
+        if a != b:
             aout.append("===DIFFVAR===")
             bout.append("===DIFFVAR===")
-            bout.append(b)
-            bout.append("===DIFFVAR===")
 
-def to_string(a, aout, aline):
-    if isinstance(a, dict):
-        for key in a.keys():
-            aout.append('{"%s":' % key)
-            aline[0] += 1
-            to_string(a[key], aout, aline)
-            aout.append('}')
-            aline[0] += 1
-    elif isinstance(a, list):
-        aout.append('[')
-        aline[0] += 1
-        for val in a:
-            to_string(val, aout, aline)
-        aout.append(']')
-        aline[0] += 1
-    else:
-        aout.append(a)
-        aline[0] += 1
+def add_begin_block(sym, aout, indent):
+    space = ' ' * indent
+    block = {}
+    lines = []
+    block["lines"] = lines
+    lines.append('%s%s' % (sym, space))
 
-def to_string2(a, aout, bout):
+def add_end_block(sym, aout, indent):
+    space = ' ' * indent
+    block = {}
+    lines = []
+    block["lines"] = lines
+    lines.append('%s%s' % (sym, space))
+
+def add_empty_block(aout):
+    block = {}
+    block["type"] = "empty"
+    block["lines"] = []
+    aout.append(block)
+
+def add_block(a, aout, indent):
+    block = {}
+    lines = []
+    block["lines"] = lines
+    to_string(a, lines, indent)
+    aout.append(block)
+
+def to_string(a, lines, indent):
+    space = ' ' * indent
     if isinstance(a, dict):
+        lines.append('%s{' % space)
+        first = True
         for key in a.keys():
-            aout.append('{"%s":' % key)
-            bout.append('===BLANK===')
-            to_string2(a[key], aout, bout)
-            aout.append('}')
-            bout.append('===BLANK===')
+            if not first:
+                lines[-1] += ','
+            lines.append('%s "%s" : ' % (space, key))
+            to_string(a[key], lines, indent + 1)
+            first = False
+        lines.append('%s}' % space)
     elif isinstance(a, list):
+        lines.append('%s[' % space)
+
+        first = True
         for val in a:
-            aout.append('[')
-            bout.append('===BLANK===')
-            to_string2(val, aout, bout)
-            aout.append(']')
-            bout.append('===BLANK===')
+            if not first:
+                lines[-1] += ','
+            to_string(val, lines, indent + 1)
+            first = False
+        lines.append('%s]' % space)
     else:
-        aout.append(a)
+        if isinstance(a, str) or isinstance(a, unicode):
+            lines.append('%s "%s"' % (space, a))
+        else:
+            lines.append('%s %s' % (space, str(a)))
+
+ 
+
+def to_string2(a, aout, bout, indent):
+    space = ' ' * indent
+    if isinstance(a, dict):
+        aout.append('%s{' % space)
+        bout.append('===BLANK===')
+        first = True
+        for key in a.keys():
+            if not first:
+                aout[-1] += ","
+            aout.append('%s "%s" : ' % (space, key))
+            bout.append('')
+            to_string2(a[key], aout, bout, indent + 1)
+            first = False
+        aout.append('%s}' % space)
+        bout.append('===BLANK===')
+    elif isinstance(a, list):
+        aout.append('%s[' % space)
+        bout.append('===BLANK===')
+        first = True
+        for val in a:
+            if not first:
+               aout[-1] += ","
+            to_string2(val, aout, bout, indent + 1)
+            first = False
+        aout.append('%s]' % space)
+        bout.append('===BLANK===')
+    else:
+        if isinstance(a, str):
+            aout.append('%s "%s"' % (space, a))
+        else:
+            aout.append('%s %s' % (space, str(a)))
         bout.append('')
